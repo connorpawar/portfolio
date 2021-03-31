2ff94f8e1954ca125336cf2b85e34f34bcb55aa9
connorpawar <pawar@ku.edu>
2021-03-30 19:11:10 -0500

    add projects page

---
+++ components/DisplayCard.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { Box, Badge, Heading, useColorMode } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+export const DisplayCard = ({
+  image,
+  imageDesc,
+  url,
+  title,
+  desc,
+  year,
+  badgeText,
+  badgeColor,
+}) => {
+  const router = useRouter();
+  const { colorMode } = useColorMode();
+
+  return (
+    <Box
+      as="button"
+      textAlign="start"
+      maxW="sm"
+      minH="xl"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      shadow="sm"
+      transition="0.2s"
+      _hover={{ shadow: "lg" }}
+      background={colorMode === "light" ? "white" : "gray.900"}
+      onClick={() => router.push(url)}
+    >
+      <img src={image} alt={imageDesc} />
+
+      <Box p="6" minH="3xs">
+        <Box d="flex" alignItems="baseline">
+          <Badge borderRadius="full" px="4" colorScheme={badgeColor}>
+            {badgeText}
+          </Badge>
+          <Box
+            color="gray.500"
+            fontWeight="semibold"
+            letterSpacing="wide"
+            fontSize="sm"
+            textTransform="uppercase"
+            ml="2"
+          >
+            {year}
+          </Box>
+        </Box>
+        <Heading
+          mt="1"
+          fontWeight="semibold"
+          as="h4"
+          size="lg"
+          lineHeight="tight"
+          isTruncated
+        >
+          {title}
+        </Heading>
+        <Box fontSize="md">{desc}</Box>
+      </Box>
+    </Box>
+  );
+};
---
+++ pages/projects.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Head from 'next/head';
+import { Heading, useColorMode, ScaleFade, Stack } from '@chakra-ui/react';
+import BackButton from '@components/BackButton';
+import { DisplayCard } from '@components/DisplayCard';
+
+import projects from '../configs/projects.json';
+
+export default function Projects() {
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Head>
+        <title>Projects - Connor Pawar</title>
+        <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/209/fire_1f525.png"
+        />
+      </Head>
+	  <BackButton />
+      <main
+        style={{
+          display: 'flex',
+          background:
+            colorMode === 'light'
+              ? `linear-gradient(rgba(255,255,255,.35), rgba(255,255,255,.35)), url('/tile.png')`
+              : `url('/tile-dark.png')`,
+          backgroundRepeat: 'repeat',
+        }}
+      >
+    <>
+      <Heading as="h2" size="xl" m="10px">
+        Most Recent Projects
+      </Heading>
+      <Stack direction={['column', 'column', 'row']} spacing={4}>
+        {projects.map((p) => {
+          return (
+            <ScaleFade initialScale={0.9} in={true}>
+              <DisplayCard
+                key={p.title}
+                year={p.year}
+				image={p.image}
+				imageDesc={p.imageDesc}
+                title={p.title}
+                desc={p.desc}
+				url={p.url}
+				badgeText={p.badgeText}
+				badgeColor={p.badgeColor}
+              />
+            </ScaleFade>
+          );
+        })}
+      </Stack>
+    </>
+      </main>
+    </>
+  );
+}
